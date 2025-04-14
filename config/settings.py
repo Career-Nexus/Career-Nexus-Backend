cf92b0a3d3865f86eb838059f70f9a11ba5d2f15
Saliu Opeyemi <saliuoazeez@gmail.com>
2025-04-14 10:23:31 +0100

    Configured backend to use Postgresql db

--- config/settings.py
+++ config/settings.py
@@ -96,12 +96,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+DATABASES ={
+    "default":{
+        "ENGINE":"django.db.backends.postgresql",
+        "NAME":"careernexus",
+        "USER":"saliuoazeez",
+        "PASSWORD":os.getenv("DATABASE_PASSWORD"),
+        "HOST":"localhost",
+        "PORT":"5432",
     }
 }
+
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 
 
 # Password validation
